b420d1c8316edc0df41dce7b78a12b11265de193
cclin <kuanyu0111@gmail.com>
2018-02-20 21:15:10 -0800

    some changes

--- Dice_rolling_simulator.py
+++ Dice_rolling_simulator.py
@@ -1,11 +1,10 @@
 import random
 def dice_rolling_simulator():
-	for i in range (1):
 		print ('The value is',random.randint(1,6))
 
 def get_yes_no(str_promt):
 	answer = input(str_promt + ' ')
-	while answer == 'yes':
+	if answer == 'yes':
 		return True
 	else:
 		if answer == 'no':
@@ -16,15 +15,10 @@ def get_yes_no(str_promt):
 			answer = input(str_promt + ' ')
 			return True
 def repeating():
-	first_time = True
 	repeat = True
 
 	while repeat:
-		if first_time:
 			print('Roll the dice!')
-		else:
-			print('Roll the dice!')
-		dice_rolling_simulator()
-		repeat = get_yes_no('DO you want to roll again?')
-		first_time = False
+			dice_rolling_simulator()
+			repeat = get_yes_no('DO you want to roll again? yes/no: ')
 repeating()
