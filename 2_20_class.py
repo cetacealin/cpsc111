b266eeb6f4aaccf3b866d82e9fa01232702660ed
cclin <kuanyu0111@gmail.com>
2018-03-13 19:44:28 -0700

    changings

--- 2_20_class.py
+++
@@ -1,9 +0,0 @@
-def string():
-    while index < len(fruit): 
-	letter = fruit[index]
-	print (letter)
-	index = index + 1
-fruit_ = input('Enter a fruit: ')
-string(fruit_)
-
-
--- trying_for_fun_exception.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-print('Let\'s fix the previous code with exception handling')
-
-try:
-    number = int(input('Enter a number between 1-10'))
-except ValueError:
-    print('Err...numbers only')
-print('you entered number',number)
