7f59e9056893859746794a0f5557916dd35a9236
cclin <kuanyu0111@gmail.com>
2018-04-02 20:45:11 -0700

    add a new filels

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,18 @@
-# Program: cpsc111BV-w18-asg1.py 
+# Program: cpsc111BV-w18-asg1.py
 # Author:Kuan 089988
 # Class: CPSC 111BV
-# Date: 01/29/2018 
-# Task: Assignment #1 
+# Date: 01/29/2018
+# Task: Assignment #1
 # Purpose: Solving simple functions learning(), totalProfit(), and getInitials()
 
-def learning(): #This is a function use to ask for user's information and then print the information 
+def learning(): #This is a function use to ask for user's information and then print the information
     name = input('What is your name?\n')
     program = input('What programming language are you learning?\n')
     print('Today, I found out that',name,'is learning a programming language called',program + '.' + '\n')
 
 learning()
 
-def totalProfit(): #This is a function designed to calculate the toatal profit from selling juice
+def totalProfit():  #This is a function designed to calculate the toatal profit from selling juice
     juice_cost_price = int(input('Enter the cost price of the juice,\n'))
     juice_sale_price = int(input('Enter the sale price of the juice,\n'))
     cans_sold = int(input('Enter the number of the cans sold,\n'))
@@ -24,16 +24,16 @@ totalProfit()
 
 def getInitials(): #This function will print my initials
     print("""
-**	 ***     **        
-**      ***      **        
-**    ***        **         
-**  ***          **          
-*****            **          
-**  ***          **          
-**    ***        **         
-**      ***      **        
-**        ***    **********              
-             
+**	 ***     **
+**      ***      **
+**    ***        **
+**  ***          **
+*****            **
+**  ***          **
+**    ***        **
+**      ***      **
+**        ***    **********
+
  """)
 
 getInitials()
